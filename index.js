210499eb6170675a5c43d47fc996eb654949f947
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2021-06-18 12:47:05 +0100

    commented out the old method of calling commands and added the new method

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-
+const fs = require('fs');
 const Discord = require("discord.js")
 const client = new Discord.Client()
 const PREFIX = "!";
 require("dotenv").config();
+// Reading command files
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
 
 client.on("ready", () => {
   console.log("This bot is online!");
@@ -11,20 +22,32 @@ client.on("ready", () => {
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
+client.on('message', message =>{
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+  const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+  if (!client.commands.has(command)) return;
 
-client.on("message", msg => {
-  let args = msg.content.substring(PREFIX.length).split(" ");
-  switch(args[0]){
-    // Ping command
-    case "ping":
-      msg.reply("pong!");
-      break;
-
-    case "yodababy":
-      msg.reply("https://media.giphy.com/media/ZFi2wFRs3lBvYNglWk/giphy.gif");
-      break;
-  }
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
 });
+// client.on("message", msg => {
+//   let args = msg.content.substring(PREFIX.length).split(" ");
+//   switch(args[0]){
+//     // Ping command
+//     case "ping":
+//       msg.reply("pong!");
+//       break;
+
+//     case "yodababy":
+//       msg.reply("https://media.giphy.com/media/ZFi2wFRs3lBvYNglWk/giphy.gif");
+//       break;
+//   }
+// });
 
 
 client.login(process.env.TOKEN);
