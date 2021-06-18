18a04a08f1debd6710e8410d90faaa62058b3d9a
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2021-06-18 14:49:52 +0100

    added a addFields section to me embed

--- commands/help.js
+++ commands/help.js
@@ -6,6 +6,9 @@ module.exports = {
 		const embed = new Discord.MessageEmbed()
         .addField('**Here are the available commands:**', "Please note that this bot uses '!' as a command prefix")
         .addField("Details:", "`!ping`**: Simple direct message that gets one responce back**")
+        .addFields(
+            { name: 'about', value: 'Some value here' }
+        )
         .setColor(0x38f560);
         message.reply(embed);
 	},
