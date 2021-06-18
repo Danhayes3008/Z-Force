b666a1dc2a3f47321427627b31d4c178319a5d6f
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2021-06-18 15:25:40 +0100

    changed the command for the help page

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 const Discord  = require('discord.js');
 module.exports = {
-	name: 'help',
+	name: 'help-about',
 	description: 'help',
 	execute(message, args) {
 		const embed = new Discord.MessageEmbed()
