5582c577cc99d649fd5b8b79d87239c92d630b75
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2021-06-21 10:31:44 +0100

    updated the code to be able to use multiple arguments in the command and added a second command for the help menu system

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,24 @@
 const Discord  = require('discord.js');
 module.exports = {
-	name: 'help-about',
+	name: 'help',
 	description: 'help',
-	execute(message, args) {
-		const embed = new Discord.MessageEmbed()
-        .addField('**Here are the available commands:**', "Please note that this bot uses '!' as a command prefix")
-        .addField("Details:", "`!ping`**: Simple direct message that gets one responce back**")
-        .addFields(
-            { name: 'about', value: 'Some value here' }
-        )
-        .setColor(0x38f560);
-        message.reply(embed);
+	execute(message, args){
+        if(!args.length) {
+            const embed = new Discord.MessageEmbed()
+            .addField('**Here are the available commands:**', "Please note that this bot uses '!' as a command prefix")
+            .addField("Details:", "`!ping`**: Simple direct message that gets one responce back**")
+            .addFields(
+                { name: 'about', value: 'Some value here' }
+            )
+            .setColor(0x38f560);
+            message.reply(embed);            
+        } else if( args[0]=== 'about'){
+            const embed = new Discord.MessageEmbed()
+            .addField('**About Commands')
+            .addField("About", "test embed")
+            .setColor(0x38f560);
+            message.reply(embed);
+        }
+		
 	},
 };
