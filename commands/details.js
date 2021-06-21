d9d2f550a7478a2098294663da7a44ca074fdca4
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2021-06-21 13:55:18 +0100

    updated the .addFields section, changed line 8 to a setTitle and created the variable version to contain the version number

--- commands/details.js
+++ commands/details.js
@@ -1,13 +1,16 @@
 const Discord  = require('discord.js');
+const version = "1.0.0";
 module.exports = {
 	name: 'details',
 	description: 'details',
 	execute(message) {
 		const embed = new Discord.MessageEmbed()
+        .setTitle("Details:")
         .addField('**Here are the available commands:**', "Please note that this bot uses '!' as a command prefix")
-        .addField("Details:")
+        
         .addFields(
-            { name: 'details', value: 'add details here' }
+            { name: 'details', value: 'add details here' },
+            { name: 'Version', value: `${version}` }
         )
         .setColor(0x38f560);
         message.reply(embed);
