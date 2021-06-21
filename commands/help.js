912b7cd1e50044c781869889a35f2cf561a9a754
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2021-06-21 10:36:53 +0100

    added comments to explain what is going on in some parts of the code

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,10 @@
 const Discord  = require('discord.js');
 module.exports = {
+    // The name is the command that is called. This one is the first one called as its the main help menu screen.
 	name: 'help',
 	description: 'help',
 	execute(message, args){
+        // If there are no arguments with the !help then this is what the bot will display to the screen
         if(!args.length) {
             const embed = new Discord.MessageEmbed()
             .addField('**Here are the available commands:**', "Please note that this bot uses '!' as a command prefix")
@@ -10,8 +12,10 @@ module.exports = {
             .addFields(
                 { name: 'about', value: 'Some value here' }
             )
+            // Sets a colour to the side of the embed box on the screen
             .setColor(0x38f560);
-            message.reply(embed);            
+            message.reply(embed);
+        // If there is one argument with the word listed in the if statement then the bot will display the content for that argument
         } else if( args[0]=== 'about'){
             const embed = new Discord.MessageEmbed()
             .addField('**About Commands')
